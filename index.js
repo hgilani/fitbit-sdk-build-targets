d977a0de8550024b70d6e60651041341abdc4d96
Himanshu Gilani <hgilani@gmail.com>
2024-09-09 19:01:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const extraBuildTargets = {
                 height: 336,
             },
         },
+    },atlas: {
+        displayName: 'Fitbit Versa 4',
+        minSDKVersion: '7.1.0',
+        platform: ['20001.1.1+'],
+        resourceFilterTag: '336x336',
+        specs: {
+            screenSize: {
+                width: 336,
+                height: 336,
+            },
+        },
     },
 };
 exports.default = extraBuildTargets;
